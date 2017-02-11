f0016a2e6ad595f850de69cb026bbe3008689cc9
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-02-11 22:31:10 +0300

    Implement bot skeleton

---
+++ bot.js
@@ -0,0 +1,121 @@
+'use strict';
+
+// States
+const states = {
+    DEFAULT: undefined,
+    WAITING_FOR_REPO: 0,
+    WAITING_FOR_CHAT_TYPE: 1,
+    WAITING_FOR_REPO_TYPE: 2,
+    WAITING_FOR_SECRET_PHRASE: 3,
+    WAITING_FOR_START: 4,
+};
+
+// Messages
+const msg = {
+    START: `I'm a Travis CI bot. I can notify you about builds of your repositories.`,
+    WAITING_FOR_REPO: `Please, send a repository name you'd like to receive build notifications from.`,
+    WAITING_FOR_REPO_TYPE: `Please, choose access level for `,
+    WAITING_FOR_CHAT_TYPE: `Please, choose where you would like to receive build notifications from `,
+    WAITING_FOR_SECRET_PHRASE: `Please, type secret phrase.`,
+    SUCCESS: 'Success',
+    UNEXPECTED_ERROR: 'Unexpected error occurred, try again.'
+};
+
+const Telegraf = require('telegraf'),
+      { Extra, Markup } = require('telegraf'),
+      db = require('./db'),
+      uuid = require('uuid/v4');
+
+const TravisCINotifications = require('./travisCINotifications');
+let travis = new TravisCINotifications();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use(Telegraf.memorySession());
+
+bot.command('start', (ctx) => {
+    ctx.reply(msg.START);
+});
+
+bot.command('link', (ctx) => {
+    ctx.session.state = states.WAITING_FOR_REPO;
+    ctx.reply(msg.WAITING_FOR_REPO);
+});
+
+bot.command('cancel', (ctx) => {
+    ctx.session.state = states.DEFAULT;
+    ctx.reply(msg.START);
+});
+
+bot.hears(/\/start@([^ ]+) (.*)/, (ctx) => {
+    let data = Buffer.from(ctx.match[2], 'base64').toString('ascii').split(':');
+    if (data.length != 2) {
+        ctx.reply(msg.UNEXPECTED_ERROR);
+        ctx.leaveChat();
+    } else {
+        db.update(data[0], data[1], ctx.chat.id).then(res => {
+            ctx.reply(msg.SUCCESS);
+        }).catch(err => {
+            console.log(err);
+            ctx.reply(msg.UNEXPECTED_ERROR);
+            ctx.leaveChat();
+        })
+    }
+});
+
+bot.on('text', (ctx) => {
+    if (ctx.session.state == states.WAITING_FOR_REPO) {
+        ctx.session.state = states.WAITING_FOR_REPO_TYPE;
+        ctx.session.uuid = uuid();
+        ctx.session.repo = ctx.message.text;
+
+        ctx.reply(msg.WAITING_FOR_REPO_TYPE + ctx.message.text, Markup.keyboard(['Public', 'Private'], {columns: 2}).oneTime().resize().extra());
+    } else if (ctx.session.state == states.WAITING_FOR_REPO_TYPE) {
+        if (['Public', 'Private'].includes(ctx.message.text)) {
+            if (ctx.message.text == 'Private') {
+                ctx.session.state = states.WAITING_FOR_SECRET_PHRASE;
+                ctx.reply(msg.WAITING_FOR_SECRET_PHRASE);
+            } else {
+                ctx.session.state = states.WAITING_FOR_CHAT_TYPE;
+                ctx.reply(msg.WAITING_FOR_CHAT_TYPE + ctx.session.repo, Markup.keyboard(['Private chat', 'Group'], {columns: 2}).oneTime().resize().extra());
+            }
+        } else {
+            ctx.reply(msg.WAITING_FOR_REPO_TYPE);
+        }
+    } else if (ctx.session.state == states.WAITING_FOR_SECRET_PHRASE) {
+        ctx.session.state = states.WAITING_FOR_CHAT_TYPE;
+        ctx.session.secretPhrase = ctx.message.text;
+
+        ctx.reply(msg.WAITING_FOR_CHAT_TYPE + ctx.session.repo, Markup.keyboard(['Private chat', 'Group'], {columns: 2}).oneTime().resize().extra());
+    } else if (ctx.session.state == states.WAITING_FOR_CHAT_TYPE) {
+        if (['Group', 'Private chat'].includes(ctx.message.text)) {
+            let id = ctx.message.text == 'Group' ? ctx.session.uuid : ctx.chat.id;
+            ctx.session.state = states.DEFAULT;
+            db.create(ctx.session.repo, id, ctx.session.secretPhrase).then(res => {
+                if (ctx.message.text == 'Group') {
+                    let payload = Buffer.from(ctx.session.repo + ':' + ctx.session.uuid).toString('base64');
+                    ctx.reply(`https://t.me/${ctx.me}?startgroup=${payload}`);
+                } else {
+                    ctx.reply(msg.SUCCESS);
+                }
+            }).catch(err => {
+                ctx.reply(msg.UNEXPECTED_ERROR);
+            });
+        } else {
+            ctx.reply(msg.WAITING_FOR_CHAT_TYPE);
+        }
+    }
+});
+
+bot.startWebhook('/telegram-webhook', null, 5001);
+
+travis.on('notify', (secret, payload) => {
+    db.get(payload.repository.name).then(record => {
+        if (record != null) {
+            if (!secret || (secret && record.secretPhrase == secret)) {
+                bot.telegram.sendMessage(record.chatId, 'Test');
+            }
+        }
+    }).catch(err => {});
+});
+
+travis.startWebhook(8080);
---
+++ db.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+let db = mongoose.createConnection(process.env.DB_URI);
+
+const recordSchema = new mongoose.Schema({
+    chatId: {
+        type: String,
+        require: true
+    },
+    repo: {
+        type: String,
+        required: true
+    },
+    secretPhrase: {
+        type: String
+    }
+});
+
+let Record = db.model('Record', recordSchema);
+
+module.exports = {
+    create: (repo, chatId, secretPhrase) => {
+        let record = Record({
+            chatId: chatId,
+            repo: repo,
+            secretPhrase: secretPhrase
+        });
+        return record.save();
+    },
+    delete: (repo, chatId) => {
+        return Record.remove({repo: repo, chatId: chatId}).exec();
+    },
+    all: (repo) => {
+        return Record.find({repo: repo}).lean().exec();
+    },
+    update: (repo, oldChatId, newChatId) => {
+        return Record.findOneAndUpdate({repo: repo, chatId: oldChatId}, {$set: {chatId: newChatId}}).lean().exec();
+    },
+    get: (repo) => {
+        return Record.findOne({repo: repo}).lean().exec();
+    }
+};
